c0a8c7d0131f14147927974777f462e87223a0b6
Faouzi Elbakri <56311060+Faouzielbakri@users.noreply.github.com>
2021-03-13 00:13:00 +0100

    profile screen completed--without payment option--

---
+++ src/Components/Signup.js
@@ -0,0 +1,57 @@
+import React, { useRef } from "react";
+import { auth } from "../firebase";
+import "./Signup.css";
+
+function Signup() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authuser) => {
+        console.log(authuser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authuser) => {
+        console.log(authuser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  return (
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input ref={emailRef} type="email" placeholder="Email Address" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="signup__grey">New to metflixy? </span>
+          <span className="signup__link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
---
+++ src/Pages/ProfileScreen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./ProfileScreen.css";
+import Nav from "./../Components/Nav";
+import { useSelector } from "react-redux";
+import { selectUser } from "./../features/userSlice";
+import { auth } from "../firebase";
+function ProfileScreen() {
+  const user = useSelector(selectUser);
+  const currentplan = "none";
+  return (
+    <div className="profilescreen">
+      <Nav />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt="profileScreen__img"
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>{`Plans (current Plan ${currentplan})`}</h3>
+              <button
+                onClick={() => auth.signOut}
+                className="profileScreen__signout"
+              >
+                Sign out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileScreen;
