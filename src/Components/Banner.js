843e58a273682a4d4a254e8e2e5b2a457041474b
Faouzi Elbakri <56311060+Faouzielbakri@users.noreply.github.com>
2021-03-15 01:34:59 +0100

    'modified banner'

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -37,7 +37,6 @@ function Banner() {
     <header
       className="banner"
       style={{
-        backgroundSize: "cover",
         backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
         backgroundPosition: "center center",
       }}
