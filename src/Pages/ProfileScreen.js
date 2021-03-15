8070d0bd62b10424fb5f8582af3627e500503792
Faouzi Elbakri <56311060+Faouzielbakri@users.noreply.github.com>
2021-03-15 02:00:55 +0100

    1st hot fix for the  error while deploying

--- src/Pages/ProfileScreen.js
+++ src/Pages/ProfileScreen.js
@@ -11,7 +11,7 @@ function ProfileScreen() {
     <div className="profilescreen">
       <Nav />
       <div className="profileScreen__body">
-        <h1>Edit Profile</h1>
+        <h1>Edit Profile .</h1>
         <div className="profileScreen__info">
           <img
             src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
